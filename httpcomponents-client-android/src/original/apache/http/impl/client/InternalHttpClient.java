3952669ceb417d972474b1fd424d2c83184db25c
Kostya Vasilyev <kmansoft@gmail.com>
2015-06-12 14:58:08 +0300

    renamed packages, added more missing files

---
+++ httpcomponents-client-android/src/original/apache/http/impl/client/InternalHttpClient.java
@@ -0,0 +1,243 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package original.apache.http.impl.client;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import android.util.Log;
+
+import original.apache.http.params.HttpParams;
+
+import original.apache.http.HttpException;
+import original.apache.http.HttpHost;
+import original.apache.http.HttpRequest;
+import original.apache.http.annotation.ThreadSafe;
+import original.apache.http.auth.AuthSchemeProvider;
+import original.apache.http.auth.AuthStateHC4;
+import original.apache.http.client.ClientProtocolException;
+import original.apache.http.client.CookieStore;
+import original.apache.http.client.CredentialsProvider;
+import original.apache.http.client.config.RequestConfig;
+import original.apache.http.client.methods.CloseableHttpResponse;
+import original.apache.http.client.methods.Configurable;
+import original.apache.http.client.methods.HttpExecutionAware;
+import original.apache.http.client.methods.HttpRequestWrapper;
+import original.apache.http.client.params.ClientPNames;
+import original.apache.http.client.params.HttpClientParamConfig;
+import original.apache.http.client.protocol.HttpClientContext;
+import original.apache.http.config.Lookup;
+import original.apache.http.conn.ClientConnectionManager;
+import original.apache.http.conn.ClientConnectionRequest;
+import original.apache.http.conn.HttpClientConnectionManager;
+import original.apache.http.conn.ManagedClientConnection;
+import original.apache.http.conn.routing.HttpRoute;
+import original.apache.http.conn.routing.HttpRoutePlanner;
+import original.apache.http.conn.scheme.SchemeRegistry;
+import original.apache.http.cookie.CookieSpecProvider;
+import original.apache.http.impl.execchain.ClientExecChain;
+import original.apache.http.params.HttpParamsNames;
+import original.apache.http.protocol.BasicHttpContextHC4;
+import original.apache.http.protocol.HttpContext;
+import original.apache.http.util.Args;
+
+/**
+ * Internal class.
+ *
+ * @since 4.3
+ */
+@ThreadSafe
+@SuppressWarnings("deprecation")
+class InternalHttpClient extends CloseableHttpClient {
+
+    private final static String TAG = "HttpClient";
+
+    private final ClientExecChain execChain;
+    private final HttpClientConnectionManager connManager;
+    private final HttpRoutePlanner routePlanner;
+    private final Lookup<CookieSpecProvider> cookieSpecRegistry;
+    private final Lookup<AuthSchemeProvider> authSchemeRegistry;
+    private final CookieStore cookieStore;
+    private final CredentialsProvider credentialsProvider;
+    private final RequestConfig defaultConfig;
+    private final List<Closeable> closeables;
+
+    public InternalHttpClient(
+            final ClientExecChain execChain,
+            final HttpClientConnectionManager connManager,
+            final HttpRoutePlanner routePlanner,
+            final Lookup<CookieSpecProvider> cookieSpecRegistry,
+            final Lookup<AuthSchemeProvider> authSchemeRegistry,
+            final CookieStore cookieStore,
+            final CredentialsProvider credentialsProvider,
+            final RequestConfig defaultConfig,
+            final List<Closeable> closeables) {
+        super();
+        Args.notNull(execChain, "HTTP client exec chain");
+        Args.notNull(connManager, "HTTP connection manager");
+        Args.notNull(routePlanner, "HTTP route planner");
+        this.execChain = execChain;
+        this.connManager = connManager;
+        this.routePlanner = routePlanner;
+        this.cookieSpecRegistry = cookieSpecRegistry;
+        this.authSchemeRegistry = authSchemeRegistry;
+        this.cookieStore = cookieStore;
+        this.credentialsProvider = credentialsProvider;
+        this.defaultConfig = defaultConfig;
+        this.closeables = closeables;
+    }
+
+    private HttpRoute determineRoute(
+            final HttpHost target,
+            final HttpRequest request,
+            final HttpContext context) throws HttpException {
+        HttpHost host = target;
+        if (host == null) {
+            host = (HttpHost) request.getParams().getParameter(ClientPNames.DEFAULT_HOST);
+        }
+        return this.routePlanner.determineRoute(host, request, context);
+    }
+
+    private void setupContext(final HttpClientContext context) {
+        if (context.getAttribute(HttpClientContext.TARGET_AUTH_STATE) == null) {
+            context.setAttribute(HttpClientContext.TARGET_AUTH_STATE, new AuthStateHC4());
+        }
+        if (context.getAttribute(HttpClientContext.PROXY_AUTH_STATE) == null) {
+            context.setAttribute(HttpClientContext.PROXY_AUTH_STATE, new AuthStateHC4());
+        }
+        if (context.getAttribute(HttpClientContext.AUTHSCHEME_REGISTRY) == null) {
+            context.setAttribute(HttpClientContext.AUTHSCHEME_REGISTRY, this.authSchemeRegistry);
+        }
+        if (context.getAttribute(HttpClientContext.COOKIESPEC_REGISTRY) == null) {
+            context.setAttribute(HttpClientContext.COOKIESPEC_REGISTRY, this.cookieSpecRegistry);
+        }
+        if (context.getAttribute(HttpClientContext.COOKIE_STORE) == null) {
+            context.setAttribute(HttpClientContext.COOKIE_STORE, this.cookieStore);
+        }
+        if (context.getAttribute(HttpClientContext.CREDS_PROVIDER) == null) {
+            context.setAttribute(HttpClientContext.CREDS_PROVIDER, this.credentialsProvider);
+        }
+        if (context.getAttribute(HttpClientContext.REQUEST_CONFIG) == null) {
+            context.setAttribute(HttpClientContext.REQUEST_CONFIG, this.defaultConfig);
+        }
+    }
+
+    @Override
+    protected CloseableHttpResponse doExecute(
+            final HttpHost target,
+            final HttpRequest request,
+            final HttpContext context) throws IOException, ClientProtocolException {
+        Args.notNull(request, "HTTP request");
+        HttpExecutionAware execAware = null;
+        if (request instanceof HttpExecutionAware) {
+            execAware = (HttpExecutionAware) request;
+        }
+        try {
+            final HttpRequestWrapper wrapper = HttpRequestWrapper.wrap(request);
+            final HttpClientContext localcontext = HttpClientContext.adapt(
+                    context != null ? context : new BasicHttpContextHC4());
+            RequestConfig config = null;
+            if (request instanceof Configurable) {
+                config = ((Configurable) request).getConfig();
+            }
+            if (config == null) {
+                final HttpParams params = request.getParams();
+                if (params instanceof HttpParamsNames) {
+                    if (!((HttpParamsNames) params).getNames().isEmpty()) {
+                        config = HttpClientParamConfig.getRequestConfig(params);
+                    }
+                } else {
+                    config = HttpClientParamConfig.getRequestConfig(params);
+                }
+            }
+            if (config != null) {
+                localcontext.setRequestConfig(config);
+            }
+            setupContext(localcontext);
+            final HttpRoute route = determineRoute(target, wrapper, localcontext);
+            return this.execChain.execute(route, wrapper, localcontext, execAware);
+        } catch (final HttpException httpException) {
+            throw new ClientProtocolException(httpException);
+        }
+    }
+
+    public void close() {
+        this.connManager.shutdown();
+        if (this.closeables != null) {
+            for (final Closeable closeable: this.closeables) {
+                try {
+                    closeable.close();
+                } catch (final IOException ex) {
+                    Log.e(TAG, ex.getMessage(), ex);
+                }
+            }
+        }
+    }
+
+    public HttpParams getParams() {
+        throw new UnsupportedOperationException();
+    }
+
+    public ClientConnectionManager getConnectionManager() {
+
+        return new ClientConnectionManager() {
+
+            public void shutdown() {
+                connManager.shutdown();
+            }
+
+            public ClientConnectionRequest requestConnection(
+                    final HttpRoute route, final Object state) {
+                throw new UnsupportedOperationException();
+            }
+
+            public void releaseConnection(
+                    final ManagedClientConnection conn,
+                    final long validDuration, final TimeUnit timeUnit) {
+                throw new UnsupportedOperationException();
+            }
+
+            public SchemeRegistry getSchemeRegistry() {
+                throw new UnsupportedOperationException();
+            }
+
+            public void closeIdleConnections(final long idletime, final TimeUnit tunit) {
+                connManager.closeIdleConnections(idletime, tunit);
+            }
+
+            public void closeExpiredConnections() {
+                connManager.closeExpiredConnections();
+            }
+
+        };
+
+    }
+
+}
